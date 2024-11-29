261c504a52e3240b76926471c2e4655332d43ab3
Fahad <97869609+fahad-ali1@users.noreply.github.com>
2024-11-28 18:42:16 -0700

    bug fixes

--- back-end/app.py
+++ back-end/app.py
@@ -29,8 +29,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://macewan-chatbot-backend.onrender.com",
-                   "http://localhost"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "OPTIONS"],
     allow_headers=["*"], 
--- back-end/main.py
+++ back-end/main.py
@@ -4,5 +4,5 @@ from app import app
 
 # Run the FastAPI app
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 6584))
+    port = int(os.environ.get("PORT", 10000))
     uvicorn.run(app, host="0.0.0.0", port=port)
