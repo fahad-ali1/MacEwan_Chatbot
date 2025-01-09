78b88da08ff3afd6fbc90660c00c3441e6a8bbfc
ManateeCalamity <bigbadseansean@gmail.com>
2025-01-08 20:20:27 -0700

    Add Example Homepage Image
    Adds example background image to the page with the chatbot

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 import Chatbot from "./components/Chatbot.js";
+import backgroundImage from './components/ExampleMacewanHomepage.jpg'
 
 const App = () => {
+  // Add example background image
+  const appStyle = {
+    backgroundImage: `url(${backgroundImage})`, 
+    backgroundSize: "cover", 
+    backgroundRepeat: "no-repeat", 
+    backgroundPosition: "center", 
+    height: "100vh", 
+    width: "100vw",
+    margin: 0, 
+  };
+
   return (
-    <div>
-      <h1>Welcome to MacEwan University</h1>
+    <div style={appStyle}>
       <Chatbot />
     </div>
   );
